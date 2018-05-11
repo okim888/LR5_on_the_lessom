3c2b905f3da6a066e5e62fb66672fb1416c25335
Vadim Lizogub <vadlvadl789@gmail.com>
2018-05-12 00:07:39 +0300

    XMLFileParser - method findWord added

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Init done");
+
+        String sampleText = "This is sample XML text, which doesn't have any XML tag.";
+
+        MyCustomFileParserInterface parser = new XMLFileParser(sampleText);
+
+        parser.findWord("XML");
+        parser.replaceWord("Example word");
+        parser.addWord("Example word");
+        parser.deleteWord("Example word");
     }
 }
--- src/XMLFileParser.java
+++ src/XMLFileParser.java
@@ -1,3 +1,39 @@
-public class XMLFileParser {
+public class XMLFileParser implements MyCustomFileParserInterface {
+    private String text;
 
+    public XMLFileParser(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public void findWord(String word) {
+        boolean founded = false;
+        String[] words = text.split(" |:|,|\\.");
+        for(String w:words){
+            if(w.equals(word)){
+                founded = true;
+                break;
+            }
+        }
+        if(founded){
+            System.out.println("XMLFileParser: Совпадение найдено!");
+        }else{
+            System.out.println("XMLFileParser: Не удалось найти слово '" + word + "'");
+        }
+    }
+
+    @Override
+    public void replaceWord(String word) {
+        System.out.println("XMLFileParser: Слово заменено");
+    }
+
+    @Override
+    public void deleteWord(String word) {
+        System.out.println("XMLFileParser: Слово удалено");
+    }
+
+    @Override
+    public void addWord(String word) {
+        System.out.println("XMLFileParser: Добавлена строка: " + word);
+    }
 }
